fb689e6e7a500e7457107be9ce28e603b8ebb16f
Meryem-S <meryemserfiraz@gmail.com>
2022-05-09 12:19:11 +0300

    URI1010-value to pay

---
+++ URI1010.py
@@ -0,0 +1,6 @@
+code1,num1,price1 = input("Enter code, number and price of product 1 #space-separated\n").split()
+print(int(code1),int(num1),"{:.2f}".format(float(price1)))
+code2,num2,price2 = input("Enter code, number and price of product 2 #space-separated\n").split()
+print(int(code2),int(num2),"{:.2f}".format(float(price2)))
+pay=(int(num1)*float(price1)+int(num2)*float(price2))
+print("Value to Pay:$ {:.2f}".format(pay))
