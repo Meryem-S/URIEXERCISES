eeb9660430e65fc4c4f6fb760c76f3ad2ae566a5
Meryem-S <meryemserfiraz@gmail.com>
2022-05-09 16:27:21 +0300

    URI1012-area

---
+++ URI1012.py
@@ -0,0 +1,11 @@
+a,b,c=input("Enter A,B and C space-separated\n").split()
+a=float(a)
+b=float(b)
+c=float(c)
+pi=3.14159
+triangle=a*c/2
+circle=pi*c**2
+trapezoid=0.5*(a+b)*c
+square=b**2
+rectangle=a*b
+print("TRIANGLE: {:.3f}".format(triangle),"CIRCLE: {:.3f}".format(circle),"TRAPEZOID: {:.3f}".format(trapezoid),"SQUARE: {:.3f}".format(square),"RECTANGLE: {:.3f}".format(rectangle),sep="\n")
