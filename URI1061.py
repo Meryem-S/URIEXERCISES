6bc6812f12333447390c9c1b7cdd8d7a8ba1bddc
Meryem-S <meryemserfiraz@gmail.com>
2022-08-01 13:58:28 +0300

    Create URI1061.py
    Event time

---
+++ URI1061.py
@@ -0,0 +1,11 @@
+day=int(input("Dia "))
+hour1,minute1,second1=map(int,input().split(":"))
+
+day2=int(input("Dia "))
+hour2,minute2,second2=map(int,input().split(":"))
+
+total1=day*86400+hour1*3600+minute1*60+second1
+total2=day2*86400+hour2*3600+minute2*60+second2
+
+
+
